dca73a697d94a7bfc1343dfadfe09550e15007b4
Matheus Diniz <matheusdiasdiniz@hotmail.com>
2023-11-24 14:22:41 -0300

    :rocket: feat: Adiciona WordCloud
    Adiciona WordCloud para cada vídeo mostrado no HTML.

--- trabalho_praticoSD/app.py
+++ trabalho_praticoSD/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, send_from_directory
 import os
 from youtubeApi import getComments, search_videos
+import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import base64
+import nltk
+from nltk.corpus import stopwords
 
 app = Flask(__name__, template_folder='template', static_url_path='')
 
@@ -13,17 +19,54 @@ def buscar_videos():
     termo_pesquisa = request.form['search']
     videos = search_videos(termo_pesquisa)[:5]
     comentarios_por_video = []
+    wordclouds_por_video = []
 
     for video in videos:
         video_id = video['video_id']
         comentarios = getComments(video_id)[:5]
-        
+
         # Adicione a URL do vídeo aos resultados
         video['url'] = f"https://www.youtube.com/watch?v={video_id}"
 
-        comentarios_por_video.append({'video': video, 'comentarios': comentarios})
+        # WordCloud para os cinco primeiros comentários de cada vídeo
+        wordcloud = WordCloud(
+            background_color='black',
+            width=800,
+            height=400,
+            stopwords=set(stopwords.words('portuguese'))
+        ).generate(' '.join(comentario['comment'] for comentario in comentarios))
 
-    return render_template("home.html", comentarios_por_video=comentarios_por_video)
+        # Adiciona os dados nas listas
+        comentarios_por_video.append({'video': video, 'comentarios': comentarios})
+        wordclouds_por_video.append({'video': video, 'wordcloud': wordcloud})
+
+    # Gera as imagens das nuvens de palavras e salva temporariamente
+    imagens_por_video = []
+    for i, wordcloud_data in enumerate(wordclouds_por_video):
+        wordcloud = wordcloud_data['wordcloud']
+        img_path = f'temp_wordcloud_{i}.png'
+        wordcloud.to_file(img_path)
+        imagens_por_video.append(img_path)
+
+    # Converte as imagens para base64 para envio para a página web
+    imagens_base64 = []
+    for img_path in imagens_por_video:
+        with open(img_path, 'rb') as img_file:
+            img_base64 = base64.b64encode(img_file.read()).decode('utf-8')
+            imagens_base64.append(img_base64)
+
+    # Remove as imagens temporárias
+    for img_path in imagens_por_video:
+        os.remove(img_path)
+
+    # Retorna HTML se a solicitação não for AJAX, senão retorna JSON
+    if request.headers.get('X-Requested-With') != 'XMLHttpRequest':
+        return render_template("home.html", comentarios_por_video=comentarios_por_video, wordclouds_por_video=wordclouds_por_video, imagens_base64=imagens_base64)
+
+    return {
+        'comentarios_por_video': comentarios_por_video,
+        'imagens_base64': imagens_base64
+    }
 
 @app.route("/about/")
 def about():
--- trabalho_praticoSD/static/script.js
+++ trabalho_praticoSD/static/script.js
@@ -6,15 +6,28 @@ function pesquisarVideos(event) {
     $.ajax({
         url: '/api/videos', // Rota para a API no Flask
         type: 'POST',
-        data: { termo: termoPesquisa },
+        data: { search: termoPesquisa },  // Corrija o nome do parâmetro para 'search'
+        dataType: 'json',  // Indica que a resposta esperada é JSON
         success: function(response) {
             var resultadosDiv = document.getElementById("resultados");
             resultadosDiv.innerHTML = ""; // Limpa o conteúdo anterior
-        
+
             // Adiciona os resultados da pesquisa ao HTML
-            for (var i = 0; i < response.resultados.length; i++) {
-                var video = response.resultados[i];
-                resultadosDiv.innerHTML += "<p>" + video.title + " - ID: " + video.video_id + "</p>";
+            for (var i = 0; i < response.comentarios_por_video.length; i++) {
+                var videoData = response.comentarios_por_video[i];
+                var wordcloudData = response.imagens_base64[i];
+
+                resultadosDiv.innerHTML += "<h2><a href='" + videoData.video.url + "' target='_blank'>" + videoData.video.title + "</a></h2>";
+                
+                // Adiciona os comentários
+                for (var j = 0; j < videoData.comentarios.length; j++) {
+                    var comentario = videoData.comentarios[j];
+                    resultadosDiv.innerHTML += "<p>" + comentario.comment + " - " + comentario.author + "</p>";
+                }
+
+                // Adiciona a Word Cloud
+                resultadosDiv.innerHTML += "<h3>Word Cloud</h3>";
+                resultadosDiv.innerHTML += "<img src='data:image/png;base64," + wordcloudData + "' alt='Word Cloud'>";
             }
         }        
     });
